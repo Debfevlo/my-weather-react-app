d1e210bb9b1ac7ac950b375823273e5d4c16d9ea
debfevlo <deborahfevlo@gmail.com>
2024-04-15 12:39:22 +0000

    working on css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import Weather from './Weather.js';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <p>weather app</p>
+        <Weather/>
         <footer>
           This project was coded by {''}
           <a href='https://github.com/Debfevlo/my-weather-react-app' target='_blank' rel='noreferrer'>Fvelo Deborah</a>{''}
